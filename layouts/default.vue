d6a5ae8425fc0d2f0031b526a132ca8d6f01af55
TylerQube <tylersmith1308@gmail.com>
2023-01-21 15:47:30 -0800

    Add global mobile view

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,16 @@
 <template>
   <v-app>
     <v-main>
-      <v-container>
-        <Nuxt />
-      </v-container>
+      <v-app-bar
+        color="black"
+        class="mb-2"
+        absolute
+      >
+        <v-toolbar-title style="color: white;">College Match</v-toolbar-title>
+      </v-app-bar>
+      <Nuxt style="margin-top: 7vh;"/>
     </v-main>
     <v-footer
-      :absolute="!fixed"
       app
     >
       <span>nwHacks 2023</span>
@@ -19,3 +23,9 @@ export default {
   name: 'DefaultLayout',
 }
 </script>
+<style>
+body {
+  max-width: min(100vw, 550px);
+  margin: auto;
+}
+</style>
