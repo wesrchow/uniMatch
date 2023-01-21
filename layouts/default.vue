b958a2182febd5d206221364bc94b5c3fc24435a
TylerQube <tylersmith1308@gmail.com>
2023-01-21 13:48:13 -0800

    init commit.

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer
+      :absolute="!fixed"
+      app
+    >
+      <span>nwHacks 2023</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card class="logo py-4 d-flex justify-center">
+        <NuxtLogo />
+      </v-card>
+      <v-card>
+        <v-card-title class="headline">
+          Welcome to College Match
+        </v-card-title>
+        <v-card-text>
+          <h2>Discover your one true love and home for the next stage of your life!</h2>
+          <h2 class="mt-2">Find exactly what you're looking for in no time flat.</h2>
+          <hr class="my-3">
+          <p>Built in 24 hours as part of nwHacks 2023.</p>
+          
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            color="primary"
+            nuxt
+            to="/inspire"
+          >
+            Continue
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
