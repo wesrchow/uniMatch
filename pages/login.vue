b3d4019175753dcc9796a8812e201256c5eefb8b
TylerQube <tylersmith1308@gmail.com>
2023-01-21 17:53:53 -0800

    Add Google Sign In Button.

---
+++ pages/login.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-container fill-height>
+      <v-row>
+        <v-spacer></v-spacer>
+        <v-col cols="12" style="display: flex; justify-content: center; align-items: center;">
+          <GoogleButton
+            @click="authLogin"
+          ></GoogleButton>
+        </v-col>
+        <v-spacer></v-spacer>
+      </v-row>
+    </v-container>
+</template>
+<script>
+export default {
+  name: 'LoginPage',
+  methods: {
+    authLogin() {
+      // login method
+    }
+  }
+}
+</script>
+<style>
+</style>
+<link type="text/css" rel="stylesheet" href="bower_components/firebaseui/dist/firebaseui.css" />
